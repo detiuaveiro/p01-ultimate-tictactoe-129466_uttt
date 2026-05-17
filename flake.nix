579553f5eed68e69dd8912f24a798df8e530ca9b
Frederico Pinto <fredmnpinto@gmail.com>
2026-05-17 09:46:56 +0100

    Update flake to work with torch and cuda

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Flappy Bird Agent - Python project";
+  description = "Ultimate Tic-Tac-Toe AI Agent";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
@@ -8,15 +8,17 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
       python = pkgs.python311;
       pythonPackages = python.pkgs;
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        name = "flappy-bird-agent";
+        name = "uttt-ai-agent";
 
-        KERAS_BACKEND = "jax";
         venvDir = "./.venv";
 
         buildInputs = [
@@ -29,17 +31,18 @@
         postVenvCreation = ''
           unset SOURCE_DATE_EPOCH
           pip install --upgrade pip
-          pip install jax jaxlib
-          pip install keras
-          pip install numpy optree
-          pip install ipython pgmpy tqdm fastapi uvicorn nltk websockets
-          pip install pytest
+          pip install torch numpy tqdm websockets pytest
         '';
 
         postShellHook = ''
           unset PYTHONPATH
           unset SOURCE_DATE_EPOCH
+
           export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib pkgs.zlib ]}:$LD_LIBRARY_PATH"
+
+          if [ -d "/run/opengl-driver/lib" ]; then
+            export LD_LIBRARY_PATH="/run/opengl-driver/lib:$LD_LIBRARY_PATH"
+          fi
         '';
       };
     };
